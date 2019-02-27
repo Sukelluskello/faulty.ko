ab32ee74680a4feca2d3ddb0dd600a42d530bfe5
Ilja Sidoroff <ilja.sidoroff@iki.fi>
2019-02-27 22:35:24 +0200

    First version of slab fault - probably not complete

--- faulty_slab.c
+++ faulty_slab.c
@@ -3,13 +3,13 @@
 #include <linux/kernel.h>
 #include <linux/debugfs.h>
 #include <linux/fs.h>
+#include <linux/slab.h>
 
 struct dentry *fil_slab;
 
 static ssize_t slab_read(struct file *fps, char *buf, size_t len, loff_t *offset);
 static ssize_t slab_write(struct file *fps, const char *buf, size_t len, loff_t *offset);
 void non_reachable_function(void);
-
 
 static const struct file_operations fops_slab = {
 	.owner = THIS_MODULE,
@@ -18,14 +18,15 @@ static const struct file_operations fops_slab = {
 	.write = slab_write,
 };
 
-
-struct node {
+struct user_controlled_data {
     char data[10];
     bool flag;
-    struct list_head list;
-};
+} *user_controlled = NULL;
 
-struct node *nodes = NULL;
+struct unrelated_data {
+    char data[10];
+} *unrelated = NULL;
+
 
 int init_slab_corruption(struct dentry *dir, const char *fn)
 {
@@ -42,21 +43,31 @@ int init_slab_corruption(struct dentry *dir, const char *fn)
 static ssize_t slab_read(struct file *fps, char *buf, size_t len,
 			loff_t *offset)
 {
-    if (nodes) {
-    }
-    else
+    if (!user_controlled) {
+	pr_debug("Faulty: Slab - Read, no data\n");
 	return 0;
+    }
 
-    return 0;
+    pr_info("Faulty: Slab - Read, there is data\n");
+    return simple_read_from_buffer(buf, len, offset,
+			    user_controlled->data, strlen(user_controlled->data));
+
 }
 
 static ssize_t slab_write(struct file *fps, const char *buf, size_t len,
 			 loff_t *offset)
 {
-    if (!nodes) {
-	// init list
+    // TODO: should have also try to have double free here
+    if (!user_controlled) {
+	pr_debug("Faulty: Slab - Write, No data\n");
+    } else {
+	pr_debug("Faulty: Slab - Write, Free old data\n");
+	kfree(user_controlled);
     }
-    return 0; // TODO implement me
+    user_controlled = kmalloc(sizeof (struct user_controlled_data), GFP_KERNEL);
+    return simple_write_to_buffer(user_controlled->data, len, offset,
+				  buf, len);
+
 }
 
 void slab_non_reachable_function(void)
